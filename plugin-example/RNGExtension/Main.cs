e328dc48610fb9675f364d8a61777e7572606a86
XezolesS <tndid7876@gmail.com>
2020-06-16 17:07:55 +0900

    Added some plugin example

---
+++ plugin-example/RNGExtension/Main.cs
@@ -0,0 +1,15 @@
+﻿using BluishOwl.Plugin;
+using System;
+
+namespace RNGExtension
+{
+    [Plugin("RNG", "XezolesS", "19.09.17")]
+    [Description("Various random number generator(RNG) for discord bot.")]
+    public class Main : Plugin
+    {
+        public override void OnLoaded()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
