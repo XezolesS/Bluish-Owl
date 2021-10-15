56f5e07399dc1677a9fba221b8dd9408ab0e8d8a
Joe K <tndid7876@gmail.com>
2021-10-16 06:25:21 +0900

    Renewal solution file and example plugin.

---
+++ plugin-example/ExamplePlugin/Main.cs
@@ -0,0 +1,14 @@
+﻿using BluishOwl.Plugin;
+using System;
+
+namespace ExamplePlugin
+{
+    [Plugin("Example Plugin", "XezolesS", "v1.0")]
+    public class Main : Plugin
+    {
+        public override void OnLoaded()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ plugin-example/ExamplePlugin/Modules.cs
@@ -0,0 +1,13 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+
+public class Modules : ModuleBase<SocketCommandContext>
+{
+	[Command("example")]
+	public async Task CommandExample()
+    {
+        await ReplyAsync("*This is the Message from Example Plugin.*");
+    }
+}
